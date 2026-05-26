e064abad66e1447bb33f3bcb528ab3804e4913d8
m04014508 <m04014508@gmail.com>
2026-05-26 10:50:17 +0000

    hh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
-import primeflixLogo from "@/assets/Primeflix.png";
 
 export function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-b from-background via-background/90 to-transparent">
-      <div className="container mx-auto h-40 relative px-4 md:px-8">
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Link to="/" aria-label="Primeflix home">
-            <img src={primeflixLogo} alt="PRIMEFLIX" className="h-36 md:h-40" />
-          </Link>
-        </div>
+    <header className="fixed top-0 left-0 right-0 z-50">
+      <div className="container mx-auto flex h-16 items-center justify-start px-4 md:px-8">
+        <Link to="/" aria-label="Primeflix home">
+          <h1 className="text-2xl font-bold tracking-widest text-red-600">
+            PRIMEFLIX
+          </h1>
+        </Link>
       </div>
     </header>
   );
